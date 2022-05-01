48ef066bffb436c693b87a45555675e2cce0075f
Nikhil.Chittem.76007611 <Nikhil.Chittem@microfocus.com>
2022-05-01 19:56:02 +0530

    adding sales code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Katalogue';
+  title = ' Katalogue';
+  firstName = 'Nikhil';
+  lastName = 'Chittem';
 }
